4ac3f2e3a98f92a6dc540bdf7e1fa8af2984a717
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-05-15 20:14:28 +0100

    chalk package added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import {add, subtract, multiply, divide } from './my_module/calculator.js';
 import readline from 'readline';
 import { stdin as input, stdout as output } from "node:process";
+ import chalk from 'chalk'
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,10 +10,10 @@ const rl = readline.createInterface({
 
 rl.question("Enter first number: ", (num1) => {
     rl.question("Enter second number: ", (num2) => {
-        console.log(`Addition result: ${add(Number(num1), Number(num2))}`);
-        console.log(`Subtraction result: ${subtract(Number(num1), Number(num2))}`);
-        console.log(`Multiplication result: ${multiply(Number(num1), Number(num2))}`);
-        console.log(`Division result: ${divide(Number(num1), Number(num2))}`);
+        console.log(chalk.green(`Addition result: ${add(Number(num1), Number(num2))}`));
+        console.log(chalk.red(`Subtraction result: ${subtract(Number(num1), Number(num2))}`));
+        console.log(chalk.blue(`Multiplication result: ${multiply(Number(num1), Number(num2))}`));
+        console.log(chalk.blue(`Division result: ${divide(Number(num1), Number(num2))}`));
         rl.close();
     });
 })
